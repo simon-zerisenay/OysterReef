cffade69758db207212a6d4313c3db088f8d2878
Eyasu Araya <128852829+EyasuTes@users.noreply.github.com>
2024-05-01 16:37:13 +0400

    added the email html

--- app/api/sendReply/route.ts
+++ app/api/sendReply/route.ts
@@ -21,12 +21,12 @@ export async function POST(req: Request, res: Response) {
     <div>
       <p>Dear ${name},</p>
       <p>Thank you for contacting us. We have received your message and will get back to you as soon as possible.</p>
-      <p>Best regards,<br/>FRC Team</p>
+      <p>Best regards,<br/>Support Team</p>
     </div>
   `;
 
   const replyMailOptions = {
-    from: '"Fujairah Research Center" <eyasuaraya0@gmail.com>',
+    from: '"Oyster reefs" <eyasuaraya0@gmail.com>',
     to: `"${name}" <${email}>`,
     subject: "Re: Your Message",
     html: replyHtml,
